11f9a81794a8473a73e1d9a3bab9a4bdd0390002
Ivstiniavs <ubilometro1@gmail.com>
2020-11-23 12:07:46 -0300

    BackEND.NET

---
+++ NovaPasta/UsuarioRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolidCursos.NovaPasta
+{
+    public class UsuarioRequest
+    {
+
+        var usuario = new Usuario(Usuario)
+            usuario.Nome = UsuarioRequest.Nome;
+            usuario.Login = UsuarioRequest.Login;
+            usuario.Senha = UsuarioRequest.Senha;
+            usuario.Email = UsuarioRequest.Email private class Email
+        {
+        }
+
+        private class Senha
+        {
+        }
+        private class Nome
+        {
+        }
+
+        private class Login
+        {
+        }
+    }
+}
---
+++ database/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SolidCursos.NovaPasta;
+
+namespace SolidCursos.database
+{
+    public class ApplicationDBContext : DbContext
+    {
+       public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) {}
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
